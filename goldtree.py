5c66247cfbfcbda1c138ac67e12967ef9681dc3d
Roni Choudhury <roni.choudhury@kitware.com>
2013-12-17 11:15:48 -0500

    Preparing for popover summarizing subtrees with disease/symptom info.

--- goldtree.py
+++ goldtree.py
@@ -85,7 +85,8 @@ def decision_tree(m):
             return Node({"name": bd[0], "discrimination": bd[1]}, go(m, with_symptom), go(m, without_symptom))
         else:
             #return Node({"name": str(map(lambda x: x[0], info)), "discrimination": None}, None, None)
-            return Node({"name": "%d: %s" % (len(diseases), str(diseases)), "discrimination": None}, None, None)
+            #return Node({"name": "%d: %s" % (len(diseases), str(diseases)), "discrimination": None}, None, None)
+            return Node({"name": diseases, "discrimination": None}, None, None)
 
     return go(m, m.diseases)
 
--- web/index.js
+++ web/index.js
@@ -1,3 +1,9 @@
+function getChildren(node){
+    return [].concat(node.children && node.children[0] ? getChildren(node.children[0]) : [],
+                     tangelo.isArray(node.symptom.name) ? node.symptom.name : [],
+                     node.children && node.children[1] ? getChildren(node.children[1]) : []);
+}
+
 $(function () {
     d3.json("decision_tree.json", function (err, data) {
         if (err) {
@@ -5,14 +11,31 @@ $(function () {
             return;
         }
 
-        console.log(data);
+        d = data;
 
         $("#tree").dendrogram({
             data: data,
+            //orientation: "vertical",
             id: {field: "id"},
-            label: {field: "symptom.name"},
-            nodesize: 15,
-            textsize: 14
+            //label: {field: "symptom.name"},
+            //nodesize: 15,
+            textsize: 14,
+            initialize: function (enter, update, exit) {
+                enter.append("title")
+                    .text(function (d) {
+                        var left,
+                            right;
+
+                        if (!tangelo.isArray(d.symptom.name)) {
+                            left = d.children && d.children[0] ? getChildren(d.children[0]) : [];
+                            right = d.children && d.children[1] ? getChildren(d.children[1]) : [];
+
+                            return d.symptom.name + "; children without: " + left + "; children with: " + right;
+                        } else {
+                            return "diseases: " + d.symptom.name;
+                        }
+                    });
+            }
         });
     });
 });
