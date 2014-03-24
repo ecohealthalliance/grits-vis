f1bf903e9d9c0ba25f75774b92873a1bb9549a57
Roni Choudhury <roni.choudhury@kitware.com>
2014-03-24 14:57:14 -0400

    Initial prototype example of multiple resize-capable divs in one page.

---
+++ web/GRITS.app/index.js
@@ -0,0 +1,23 @@
+/*jslint browser: true */
+/*globals d3, $ */
+
+$(function () {
+    "use strict";
+
+    var resize = function () {
+        var width = $(this).width(),
+            height = $(this).height();
+
+        d3.select(this)
+            .html("<b>width: </b>" + width + "<br>" +
+                  "<b>height: </b>" + height + "<br>");
+    };
+
+    $(window).resize(function () {
+        $("div").trigger("resize.div");
+    });
+    $(".content").on("resize.div", resize);
+
+    // Create control panel.
+    $("#control-panel").controlPanel();
+});
